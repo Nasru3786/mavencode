ef208a2d31386bc690bc632bc7ebf1e86bf222af
Nasru3786 <112463279+Nasru3786@users.noreply.github.com>
2022-10-17 21:23:34 +0530

    Delete subnet.tf

--- subnet.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_subnet" "main" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1b"
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "public_subnet"
-  }
-}
